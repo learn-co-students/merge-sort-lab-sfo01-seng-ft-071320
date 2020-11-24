95dc6f485b3b59e2bb79a5a0b3f99f7d2a38bdbf
keeks5456 <Akikogreen08@gmail.com>
2020-11-24 15:38:19 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
+
+function merge(firstSubarray, secondSubArray){
+  let sorted = []
+  while(firstSubarray.length != 0 && secondSubArray.length != 0){
+    if(firstSubarray[0] < secondSubArray[0]){
+      sorted.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubArray))
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubArray)
+}
+function mergeSort(array){
+    let midpoint = array.length / 2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sorted;
+
+
+    if(firstHalf < 2){
+        return array 
+        //if it is less than 2 b/c then it is already sorted
+    } else {
+         sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted
+}
